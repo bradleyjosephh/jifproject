9eb6aec8f68da883792d30f08fe1acebe6fae7bf
bradleyjosephh <bradleyjosephh@gmail.com>
2021-12-15 13:21:32 -0800

    completed jif

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2')
 
-const db = mysql.createConnection('mysql://root:rootroot@localhost:3301/jif_db')
+const db = mysql.createConnection('mysql://root:rootroot@localhost:3306/jif_db')
 
 module.exports = db
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const mysql = require('mysql2')
 
 let app = express();
 
-app.use(express.static(path.join(__dirname, 'public/')))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+
+
+app.use(require('./routes'))
 
 app.listen(3001);
